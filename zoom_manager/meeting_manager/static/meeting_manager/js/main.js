50f83d1c791e9202d724a2817188d940a15b7bc9
Evgenyi Shuvaev <evgenyishuvaevmac@yandex.ru>
2022-01-23 22:07:53 +0300

    Add temlates and static files

---
+++ zoom_manager/meeting_manager/static/meeting_manager/js/main.js
@@ -0,0 +1,6 @@
+let refreshButton = document.querySelector("#refreshTabelButton")
+refreshButton.addEventListener("click", refreshSheduledMeetings, false)
+
+function refreshSheduledMeetings() {
+    alert("Was Action!!!")
+}
--- zoom_manager/meeting_manager/views.py
+++ zoom_manager/meeting_manager/views.py
@@ -1,7 +1,7 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def meetings_page(request):
-    return HttpResponse("Zoom manager")
+    return render(request, "meeting_manager/index.html")
